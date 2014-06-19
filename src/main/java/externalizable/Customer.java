696a40c55ea47d5921031cbd6bbf0bb395e4b6d2
Mustafa Orkun Acar <mustafaorkunacar@gmail.com>
2014-06-19 16:24:19 +0300

    externizable implemented

--- src/main/java/externalizable/Customer.java
+++ src/main/java/externalizable/Customer.java
@@ -1,34 +1,49 @@
 /**
- * Created by Esref Ozturk <esrefozturk93@gmail.com> on 18.06.2014.
+ * Created by Esref Ozturk <esrefozturk93@gmail.com> on 18.06.2014. Cool.
  */
 
 package externalizable;
-
-import com.hazelcast.nio.ObjectDataOutput;
 
 import java.io.Externalizable;
 import java.io.ObjectOutput;
 import java.io.ObjectInput;
 import java.io.IOException;
+import java.text.Format;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.lang.ClassNotFoundException;
+import java.util.ArrayList;
 
-public class Customer implements Externalizable {
+public class Customer implements Externalizable
+{
     public enum Sex { MALE, FEMALE }
     String name;
     Date birthday;
     Sex gender;
     String emailAddress;
-    long[] longArray;
+    ArrayList<Long> longArray;
 
-    public void writeExternal(ObjectOutput out) throws IOException{
-
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException
+    {
+        name = in.readLine();
+        birthday = new Date(Long.valueOf(in.readLine()).longValue());
+        gender = Sex.valueOf(in.readLine());
+        emailAddress = in.readLine();
+        int len = in.readInt();
+        longArray = new ArrayList<Long>(len);
+        Long tmp;
+        for(int i = 0; i < len; i++)  {  tmp = in.readLong(); longArray.add(tmp); }
     }
 
-    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException{
-
+    public void writeExternal(ObjectOutput out) throws IOException
+    {
+        out.writeChars(name);
+        Format formatter = new SimpleDateFormat("yyyy-dd-MM hh:mm:ss");
+        String s = formatter.format(birthday);
+        out.writeChars(s);
+        out.writeChars(gender.toString());
+        out.writeChars(emailAddress);
+        out.writeInt(longArray.size());
+        for(int i = 0; i < longArray.size(); i++) out.writeLong(longArray.get(i));
     }
-
-
-
 }
